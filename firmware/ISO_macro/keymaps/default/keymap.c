9e9dfb50fdb3b9459f78fd696a1f08744218ad88
arnstadm <45672752+arnstadm@users.noreply.github.com>
2021-06-25 22:03:10 +0200

    underglow (#5)

--- firmware/ISO_macro/keymaps/default/keymap.c
+++ firmware/ISO_macro/keymaps/default/keymap.c
@@ -2,18 +2,13 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _BASE,
-    _FN
+    _BASE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_1, KC_2, KC_3, KC_ENT,
         KC_TAB, KC_4, KC_5, KC_6
-    ),
-    [_FN] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
 
