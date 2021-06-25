be0ae4cf8d673e7a621234d4331767e98dab6f78
arnstadm <45672752+arnstadm@users.noreply.github.com>
2021-06-25 21:19:46 +0200

    underglow

--- firmware/ISO_macro/config.h
+++ firmware/ISO_macro/config.h
@@ -7,41 +7,28 @@
 #define VENDOR_ID    0xF055
 #define PRODUCT_ID   0x1234
 #define DEVICE_VER   0x0001
-#define MANUFACTURER Jakob Grønhaug
-#define PRODUCT      ISO_macro
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 4
+#define MANUFACTURER JG
+#define PRODUCT      ISO_MACRO
+
 
 #define DIRECT_PINS {{D1, D2, D3, B0}, {C6, D4, D5, D6}}
-#define UNUSED_PINS
 
 #define ENCODERS_PAD_A {C2}
 #define ENCODERS_PAD_B {D0}
 
-#define BACKLIGHT_PIN D7
-#define BACKLIGHT_LEVELS 3
-#define BACKLIGHT_BREATHING
 
-#define RGB_DI_PIN E2
+#define RGB_DI_PIN D7
 #ifdef RGB_DI_PIN
-#    define RGBLED_NUM 16
+#    define RGBLED_NUM 6
 #    define RGBLIGHT_HUE_STEP 8
 #    define RGBLIGHT_SAT_STEP 8
 #    define RGBLIGHT_VAL_STEP 8
 #    define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-#    define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
 
 //#    define RGBLIGHT_ANIMATIONS
 
-#    define RGBLIGHT_EFFECT_BREATHING
 #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
+
 /*== customize breathing effect ==*/
 /*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
 #    define RGBLIGHT_BREATHE_TABLE_SIZE 64      // 256(default) or 128 or 64
@@ -58,5 +45,3 @@
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 
-#define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 3
